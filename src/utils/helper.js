6c89bf091f12e3c9efa875643fa0fd29821bc351
02utkarsh <utkarsh7424sharma@gmail.com>
2024-07-10 19:31:52 +0530

    changes

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,17 +1,18 @@
 import { GithubAuthProvider, signInWithRedirect } from 'firebase/auth';
+import { signInWithPopup } from 'firebase/auth';
 import {auth} from "../config/firebase.config";
 import { GoogleAuthProvider } from 'firebase/auth';
 import {v4 as uuidv4} from "uuid";
 const googleProvider=new GoogleAuthProvider()
 const githubProvider= new GithubAuthProvider()
 export const signINWithGoogle= async()=>{
-    await signInWithRedirect(auth,googleProvider).then((userCred) =>{
+    await signInWithPopup(auth, new GoogleAuthProvider()).then((userCred) =>{
         console.log(userCred);
         window.location.reload();
     });
 }
 export const signINWithGithub= async()=>{
-    await signInWithRedirect(auth,githubProvider)
+    await signInWithPopup(auth, new GithubAuthProvider())
     .then((userCred) =>{
         console.log(userCred)
         window.location.reload();
